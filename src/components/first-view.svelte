bd77ce97a67b5904ec0a9cacb40d8dbe1a4e4e4e
teruyakusumoto <teruyakusumoto@gmail.com>
2024-08-24 01:14:06 +0900

    font変更

--- src/components/first-view.svelte
+++ src/components/first-view.svelte
@@ -8,12 +8,12 @@
     class="absolute top-0 left-0 w-full h-full"
     style="background-color: rgba(0, 0, 0, 0.4);"
   />
-  <div class="quicksand absolute top-0 left-0 w-full h-full text-white flex flex-col justify-between pt-12 pb-6">
+  <div class="cinzel absolute top-0 left-0 w-full h-full text-white flex flex-col justify-between pt-12 pb-6">
     <h1 class="text-center text-4xl mb-8">
       INVITATION
     </h1>
     <div>
-      <h2 class="text-center text-2xl mb-4">
+      <h2 class="text-center text-3xl mb-4">
         TERUYA & AYANA
       </h2>
       <h3 class="text-center text-xl">
