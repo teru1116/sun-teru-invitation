2a64d9542a0f1a58bd729cf9546f66ad8de14430
teruyakusumoto <teruyakusumoto@gmail.com>
2024-08-25 11:43:50 +0900

    一度ページを離れても前回のフォームの入力内容を復元

--- src/components/answer-form.svelte
+++ src/components/answer-form.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+  import { onMount } from "svelte";
   import PostalCodeInput, { type PostalCodeInputChangeEvent } from "./postal-code-input.svelte";
   import PrefectureSelect from "./prefecture-select.svelte";
   import RadioButtonGroup from "./radio-button-group.svelte";
   import ArrowForwardSvg from "./icons/arrow-forward-svg.svelte";
+  
+  onMount(() => restoreFormData())
 
   let willAttend: boolean | null = null
   let phoneNumber = ''
@@ -12,6 +15,8 @@
   let prefecture = ''
   let address = ''
   let willUseShuttleBus: boolean | null = null
+
+  $: disabled = willAttend === null || !phoneNumber || !email || !birthday || !postalCode || !prefecture || !address || willUseShuttleBus === null
 
   function onWillAttendChange(event: CustomEvent<boolean>) {
     willAttend = event.detail
@@ -31,7 +36,25 @@
     willUseShuttleBus = event.detail
   }
 
-  $: disabled = willAttend === null || !phoneNumber || !email || !birthday || !postalCode || !prefecture || !address || willUseShuttleBus === null
+  function saveFormDataToLocal() {
+    const prefix = 'SunTeruWeddingInvitation.FormData.'
+    localStorage.setItem(`${prefix}phoneNumber`, phoneNumber)
+    localStorage.setItem(`${prefix}email`, email)
+    localStorage.setItem(`${prefix}birthday`, birthday)
+    localStorage.setItem(`${prefix}postalCode`, postalCode)
+    localStorage.setItem(`${prefix}prefecture`, prefecture)
+    localStorage.setItem(`${prefix}address`, address)
+  }
+
+  function restoreFormData() {
+    const prefix = 'SunTeruWeddingInvitation.FormData.'
+    phoneNumber = localStorage.getItem(`${prefix}phoneNumber`) ?? ''
+    email = localStorage.getItem(`${prefix}email`) ?? ''
+    birthday = localStorage.getItem(`${prefix}birthday`) ?? ''
+    postalCode = localStorage.getItem(`${prefix}postalCode`) ?? ''
+    prefecture = localStorage.getItem(`${prefix}prefecture`) ?? ''
+    address = localStorage.getItem(`${prefix}address`) ?? ''
+  }
 </script>
 
 <form>
@@ -46,34 +69,34 @@
 
   <fieldset class="required">
     <legend>電話番号</legend>
-    <input type="tel" name="電話番号" placeholder="09012345678" required bind:value={phoneNumber}>
+    <input type="tel" name="電話番号" placeholder="09012345678" required bind:value={phoneNumber} on:blur={saveFormDataToLocal}>
   </fieldset>
 
   <fieldset class="required">
     <legend>メールアドレス</legend>
-    <input type="email" name="email" placeholder="example@gmail.com" autocomplete="email" required bind:value={email}>
+    <input type="email" name="email" placeholder="example@gmail.com" autocomplete="email" required bind:value={email} on:blur={saveFormDataToLocal}>
   </fieldset>
 
   <fieldset class="required">
     <legend>誕生日</legend>
-    <input type="email" name="email" placeholder="11月16日" autocomplete="email" required bind:value={birthday}>
+    <input type="email" name="email" placeholder="11/16" autocomplete="email" required bind:value={birthday} on:blur={saveFormDataToLocal}>
   </fieldset>
 
   <fieldset class="required">
     <legend>郵便番号</legend>
-    <PostalCodeInput postalCode={postalCode} on:change={onPostalCodeChange} />
+    <PostalCodeInput postalCode={postalCode} on:change={onPostalCodeChange} on:blur={saveFormDataToLocal} />
   </fieldset>
 
   <fieldset class="required">
     <legend class="mb-2">都道府県</legend>
     <!-- 郵便番号のオートコンプリートにより、市区町村までの住所が補完されるため、都道府県と市区町村のinputはautocompleteを設定していない -->
     <input type="text" name="都道府県" class="hidden" value={prefecture}>
-    <PrefectureSelect prefecture={prefecture} on:change={onPrefectureSelect} />
+    <PrefectureSelect prefecture={prefecture} on:change={onPrefectureSelect} on:blur={saveFormDataToLocal} />
   </fieldset>
 
   <fieldset class="required">
     <legend>住所</legend>
-    <input type="text" name="住所" placeholder="千代田区千代田1-1" required bind:value={address}>
+    <input type="text" name="住所" placeholder="千代田区千代田1-1" required bind:value={address} on:blur={saveFormDataToLocal}>
   </fieldset>
 
   <fieldset class="required">
