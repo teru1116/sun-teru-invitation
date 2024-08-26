7e829c99b8d3ac4e5f819cd73f0c1723fd46b2bf
teruyakusumoto <teruyakusumoto@gmail.com>
2024-08-26 19:49:15 +0900

    ビルド時に環境変数を注入

--- src/routes/thanks/+page.svelte
+++ src/routes/thanks/+page.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+  import { STORAGE_KEY_PREFIX } from "../../const";
   import AccessList from "../../components/access-list.svelte";
   import InformationList from "../../components/information-list.svelte";
   import GoogleMapSection from "../../components/googlemap.svelte";
   import FooterDefault from "../../components/footer-default.svelte";
   import GuestProfile from "../../components/guest-profile.svelte";
+
+  onMount(() => {
+    // デバッグ用 後で消す
+    console.log(document.referrer)
+    console.log(import.meta.env.VITE_FORM_REDIRECT_FROM)
+
+    const formRedirected = document.referrer && document.referrer.startsWith(import.meta.env.VITE_FORM_REDIRECT_FROM)
+    if (formRedirected) {
+      localStorage.setItem(`${STORAGE_KEY_PREFIX}completedAt`, String(new Date().getTime()))
+    }
+  })
 </script>
 
 <div class="bg-bgGray min-h-screen">
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,10 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_FORM_ENDPOINT: string;
+  readonly VITE_FORM_ENDPOINT: string
+  readonly VITE_FORM_REDIRECT_FROM: string
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv;
+  readonly env: ImportMetaEnv
 }
