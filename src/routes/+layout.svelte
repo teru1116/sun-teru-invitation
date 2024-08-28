55871d77a4ce37331f5b1978d500f475bcf6d6a8
teruyakusumoto <teruyakusumoto@gmail.com>
2024-08-28 10:58:20 +0900

    起動時にURLクエリパラメータを取得

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,22 @@
-<script>
+<script lang="ts">
   import "../app.css";
+  import { onMount } from "svelte"
+
+  let g: string | null = null
+
+  onMount(() => {
+    const url = new URL(location.href)
+    const queryParams = new URLSearchParams(url.search)
+    g = queryParams.get('g')
+  })
+
+  function init() {
+    // URLクエリパラメータに付与されたゲストIDから氏名を取得する
+
+    // URLからクエリパラメータを取り除く
+  }
+
+  init()
 </script>
 
 <slot />
