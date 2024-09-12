868e8e7f9545e7dbefefa8d9ea89a9944e4f2407
teruyakusumoto <teruyakusumoto@gmail.com>
2024-09-13 01:02:37 +0900

    動画反映

--- src/components/first-view.svelte
+++ src/components/first-view.svelte
@@ -1,7 +1,7 @@
 <script lang="ts"></script>
 
 <div class="h-[440px] relative">
-  <video id="fv-video" class="absolute top-0 left-0 w-full h-[440px] object-cover" autoplay loop muted playsinline>
+  <video id="fv-video" class="absolute top-0 left-0 w-full h-[440px] object-cover scale-[1.3] object-[56%_50%] sm:scale-[1.0] sm:object-[50%_30%]" autoplay loop muted playsinline>
     <source src="/firstview.mp4" type="video/mp4">
   </video>
   <div
